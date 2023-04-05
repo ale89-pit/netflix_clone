864a740f528e4b81f738e278a9664a7713649718
ale89-pit <72412290+ale89-pit@users.noreply.github.com>
2023-04-05 20:59:30 +0200

    ultime modifiche

--- src/componet/FilmList.jsx
+++ src/componet/FilmList.jsx
@@ -4,19 +4,19 @@ import  Container  from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Carousel, CarouselItem } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-class FilmList extends Component{
-  state ={
-    film : []
-  }
-  
-render(){
+const FilmList = (props) =>{
+
+  const [film, setFilm] = useState([])
+
 return(
 
     
            
-    <Col className="img-fluid m-1" key={this.props.film._id}>
-    <img id="copertina"  src={this.props.film.Poster} />
+    <Col className="img-fluid my-5">
+   <Link to={"/movie-details/"+ props.film.imdbID}> <img id="copertina"  src={props.film.Poster} /></Link>
    </Col>
       
                    
@@ -26,7 +26,7 @@ return(
     
 
 )
-                }
+                
 }
 
 export default FilmList
